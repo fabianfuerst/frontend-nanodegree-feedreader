4b1b9b73d255b0b7f5ac343e709a4797bcbffaed
Fabian Fürst <fa.fuerst@gmail.com>
2017-10-03 13:53:32 +0200

    add RSS Feeds and menu test suites

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,29 +31,49 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('each have a defined URL', function(){
+          for(var i = 0; i<allFeeds.length; i++) {
+            expect(allFeeds[i].url).toBeDefined();
+            expect(allFeeds[i].url).not.toBe(0);
+          };
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('each have a defined name', function(){
+           for(var i = 0; i<allFeeds.length; i++) {
+             expect(allFeeds[i].name).toBeDefined();
+             expect(allFeeds[i].name).not.toBe(0);
+           };
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('should be hidden by default', function(){
+           expect(document.getElementsByClassName("menu-hidden")).not.toBe(0);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+          it('should change visibility when clicked', function(){
+            $('body').toggleClass('menu-hidden');
+            expect($('body').attr('class')).toBe('');
+            $('body').toggleClass('menu-hidden');
+            expect($('body').attr('class')).toBeDefined();
+          });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
